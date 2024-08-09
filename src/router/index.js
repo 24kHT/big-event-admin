b61c3f8bd102594b52d90b4265cf0be75dfa6457
itheima <759110735@qq.com>
2024-08-09 23:25:26 +0800

    导航守卫配置完成了

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+import { useUserStore } from '@/stores'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -35,6 +36,15 @@ const router = createRouter({
       ]
     }
   ]
+})
+
+// 配置导航守卫
+
+router.beforeEach((to) => {
+  const useStore = useUserStore()
+  if (!useStore.token && to.path !== '/login') {
+    return '/login'
+  }
 })
 
 export default router
