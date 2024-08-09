207a2148a8a8518033062d997bc0ada8c7a2fb26
itheima <759110735@qq.com>
2024-08-09 23:12:14 +0800

    配置了路由

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,22 @@ const router = createRouter({
         {
           path: '/article/manage',
           component: () => import('@/views/article/ArticleManage.vue')
+        },
+        {
+          path: '/article/channel',
+          component: () => import('@/views/article/ArticleChannel.vue')
+        },
+        {
+          path: '/user/profile',
+          component: () => import('@/views/user/UserProfile.vue')
+        },
+        {
+          path: '/user/avatar',
+          component: () => import('@/views/user/UserAvatar.vue')
+        },
+        {
+          path: '/user/password',
+          component: () => import('@/views/user/UserPassword.vue')
         }
       ]
     }
---
+++ src/views/user/UserPassword.vue
@@ -0,0 +1,1 @@
+<template>我是password</template>
