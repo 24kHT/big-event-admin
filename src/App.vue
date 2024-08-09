be6868b30a11765820cf2b8db8c4d174f645964d
itheima <759110735@qq.com>
2024-08-09 09:47:18 +0800

    登录注册页面搭建

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
-<script setup>
-import { useUserStore } from '@/stores/index'
-
-const useStore = useUserStore()
-</script>
+<script setup></script>
 
 <template>
-  <div>我是app</div>
-  <p>{{ useStore.token }}</p>
-  <div>
-    <el-button type="success" @click="useStore.setToken">登录</el-button>
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
