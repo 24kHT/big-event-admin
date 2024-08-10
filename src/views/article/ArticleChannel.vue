2aeb2544a92c1cef28bab9ba9ae6362d896305f5
itheima <759110735@qq.com>
2024-08-10 18:06:21 +0800

    文章框架组件添加主体部分

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -5,5 +5,6 @@ import PageContainer from '@/components/PageContainer.vue'
 <template>
   <PageContainer title="文章分类">
     <template #extra><el-button type="primary">添加分类</el-button></template>
+    文章内容+内容
   </PageContainer>
 </template>
