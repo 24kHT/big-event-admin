10e8936feeb495c38f27fb065386332a8bbcb011
itheima <759110735@qq.com>
2024-08-10 12:18:49 +0800

    架子下拉功能完成

--- src/stores/modules/user.js
+++ src/stores/modules/user.js
@@ -5,21 +5,29 @@ import { ref } from 'vue'
 export const useUserStore = defineStore(
   'big-user',
   () => {
+    // 鉴权码
     const token = ref()
     const setToken = (tokenValue) => {
       token.value = tokenValue
     }
 
+    // 获取从存储用户信息
     const user = ref({})
     const getUser = async () => {
       const res = await userGetInfoService() // 请求获取数据
       user.value = res.data.data
     }
+
+    // 删除用户信息
+    const deleteUser = () => {
+      user.value = {}
+    }
     return {
       token,
       setToken,
       user,
-      getUser
+      getUser,
+      deleteUser
     }
   },
   {
--- src/views/layout/LayoutContainer.vue
+++ src/views/layout/LayoutContainer.vue
@@ -12,12 +12,26 @@ import {
 import avatar from '@/assets/default.png'
 import { useUserStore } from '@/stores'
 import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 
 // 获取用户信息
 const userStore = useUserStore()
 onMounted(() => {
   userStore.getUser()
 })
+
+// 下拉菜单跳转路由
+const router = useRouter()
+const handleCommand = (value) => {
+  // console.log(value)
+  // 退出登录
+  if (value === 'logout') {
+    userStore.setToken('')
+    userStore.deleteUser()
+    router.push('/login')
+  }
+  router.push(`/user/${value}`)
+}
 </script>
 
 <template>
@@ -66,7 +80,7 @@ onMounted(() => {
             userStore.user.nickname || userStore.user.username
           }}</strong>
         </div>
-        <el-dropdown placement="bottom-end">
+        <el-dropdown placement="bottom-end" @command="handleCommand">
           <span class="el-dropdown__box">
             <el-avatar :src="userStore.user.user_pic || avatar" />
             <el-icon><CaretBottom /></el-icon>
