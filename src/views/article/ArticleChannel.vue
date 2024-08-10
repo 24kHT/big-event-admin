46351120e2fb891c49423a3aededb9b77f79f760
itheima <759110735@qq.com>
2024-08-10 18:23:08 +0800

    文章分类页面获取分类列表

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -1,5 +1,15 @@
 <script setup>
+import { artGetChannelsService } from '@/api/article'
 import PageContainer from '@/components/PageContainer.vue'
+import { ref } from 'vue'
+
+const artList = ref([])
+// 请求文章分类列表
+const getArtList = async () => {
+  const res = await artGetChannelsService()
+  artList.value = res.data.data
+}
+getArtList()
 </script>
 
 <template>
