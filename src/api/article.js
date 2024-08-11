66458c498a26080c0710b99dcfb920a018038f89
itheima <759110735@qq.com>
2024-08-11 08:44:40 +0800

    封装了增加文章分类请求

--- src/api/article.js
+++ src/api/article.js
@@ -1,3 +1,11 @@
 // 获取文章分类列表
 import request from '@/utils/request'
 export const artGetChannelsService = () => request.get('/my/cate/list')
+
+// 增加文章分类
+export const artAddChanenelService = (cate_name, cate_alias) => {
+  return request.post('/my/cate/add', {
+    cate_name,
+    cate_alias
+  })
+}
